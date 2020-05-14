d2209b34dc7f63dd91643db8099348c8a1007a80
R Levels <rlevels@kent.edu>
2020-05-14 03:07:16 -0400

    first commit

---
+++ Javascript/hello.js
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+
+<body>
+    <p>Before the script...</p>
+
+    <script>
+        alert('Hello World!');
+    </script>
+
+    <p>...After the script.</p>
+</body>
+
+</html>

6f4fb85c70f50b7c9bbb1b68e1eb62d55ab30aad
R Levels <rlevels@kent.edu>
2020-05-24 22:50:31 -0400

    worked on swift intro

--- Swift/Hello/Package.swift
+++ Swift/Hello/Package.swift
@@ -5,12 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "Hello",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Hello",
-            targets: ["Hello"]),
-    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),

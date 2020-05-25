9277eec53ea5b1e716d9189a6d6b8368b6fb3adb
R Levels <rlevels@kent.edu>
2020-05-25 00:54:46 -0400

    completed lesson 1

---
+++ Swift/CodeWithChris/Lesson 1/Variables.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playasdrf"
+
+print(str)
+
+var a = 1 // variable declaration
+var b = 1
+
+a = 2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+let c = 10 // constant

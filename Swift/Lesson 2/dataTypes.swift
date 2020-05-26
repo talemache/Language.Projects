0c0f3abe2cf19c00d6da4bea27dda754c93e9dc9
R Levels <rlevels@kent.edu>
2020-05-25 23:48:46 -0400

    completed lesson 2

---
+++ Swift/Lesson 2/dataTypes.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var str: String = "Hello, playasdrf" 
+// var str: Int = "Hello" not allowed
+
+print(str)
+
+var a: Int = 1 // variable declaration
+var b: Int = 1
+
+a = 2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+let c = 10 // constant

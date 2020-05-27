24fba1891e6e2c7e065527be03831e267b6ce399
R Levels <rlevels@kent.edu>
2020-05-26 23:33:30 -0400

    demo server created

--- Angular/Angular Fast Crash Course/demo/README.md
+++ Angular/Angular Fast Crash Course/demo/README.md
@@ -1,4 +1,4 @@
-# Demo
+# Demo2
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.3.
 
--- Angular/Angular Fast Crash Course/demo/src/app/app.component.spec.ts
+++ Angular/Angular Fast Crash Course/demo/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'demo'`, () => {
+  it(`should have as title 'demo2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('demo');
+    expect(app.title).toEqual('demo2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('demo app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('demo2 app is running!');
   });
 });

471eaef1e6472afac41568afbe0d09c32d414c60
Ryan Levels <rlevels@Ryans-MacBook-Pro.local>
2020-10-30 16:40:22 -0400

    removed deprecated code

---
+++ Java/Discord/codeBlock.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        logger.log("This will be printed");
+    }
+}
--- Java/HTML/HelloWorldApplet.java
+++
@@ -1,8 +0,0 @@
-import java.awt.Graphics;
-
-class HelloWorldAppet extends java.applet.Applet{
-
-    public void paint(Graphics g){
-        g.drawString("Hello World!", 5, 25);
-    }
-}

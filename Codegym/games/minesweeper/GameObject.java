f275e5f262df22cdfbcc23839e7ed8f510719240
talemache <ryan@starvingdev.blog>
2020-12-19 23:59:14 -0500

    worked in MineSweeper game

---
+++ Codegym/games/minesweeper/GameObject.java
@@ -0,0 +1,11 @@
+package com.codegym.games.minesweeper;
+
+public class GameObject {
+    public int x;
+    public int y;
+
+    GameObject(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
